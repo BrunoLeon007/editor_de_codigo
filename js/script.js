a2a55bfeb843523900f63e586ca14f7cea086014
BrunoLeon007 <brunob734@gmail.com>
2024-10-29 21:25:42 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var nome =  (prompt("Digite seu nome"))
+var idade = (prompt("Digite sua idade"))
+var curso = (prompt("Digite seu curso"))
+var profissao = (prompt("profissao"))
+
+console.log(nome)
+console.log(idade)
+console.log(curso)
+console.log(profissao)
+
+
+alert(`
+    Seu Nome: ${nome}
+    Sua idade: ${idade}
+    Seu curso: ${curso}
+    Sua Profissao: ${profissao}`)
+
+
